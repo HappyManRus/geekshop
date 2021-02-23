50e2305397fc70104adbbc5e2b147fa8f8420e82
HappyManRus <62791404+HappyManRus@users.noreply.github.com>
2021-02-23 02:16:43 -0800

    Lesson 6 (#6)
    * Lesson 6. Basket extension
    
    * Lesson 6. Hot product
    
    * Lesson 6. Basket read and delete
    
    * Lesson 6. Basket read and delete 2
    
    * Lesson 6. Product page
    
    * Lesson 6. Product page 2
    
    * Lesson 6. Login fix
    
    * Lesson 6. After login
    
    * Lesson 6. AJAX
    
    * Lesson 6. AJAX_plus

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,9 +6,6 @@ app_name = "mainapp"
 
 urlpatterns = [
     path("", mainapp.products, name="index"),
-<<<<<<< HEAD
     path("category/<int:pk>/", mainapp.products, name="category"),
-=======
-    path("<int:pk>/", mainapp.products, name="category"),
->>>>>>> 3f1c69c0bcb100de897fe6af20939e0fd64d0777
+    path("product/<int:pk>/", mainapp.product, name="product"),
 ]
