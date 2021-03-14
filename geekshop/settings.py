8c759b20b6eec7785d6b92432a26c1b60236d01d
HappyManRus <62791404+HappyManRus@users.noreply.github.com>
2021-03-14 20:45:27 +0300

    Lesson 11 (#17)
    * lesson_11. Dropdown
    
    * lesson_11. Ordersapp

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
+
 
 # Auth model
 AUTH_USER_MODEL = "authnapp.ShopUser"
@@ -74,6 +76,7 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
